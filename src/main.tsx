82cfa334ed81f2f15969cf3a619b0dff74460757
AbdujalilovIsmoil <ismoilbek20050421@gmail.com>
2023-07-28 23:36:47 +0300

    feat: tanstack query and formik libraries added

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
 import App from "./App.tsx";
 import "./assets/styles/index.scss";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+const client = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <>
+    <QueryClientProvider client={client}>
+      <App />
+    </QueryClientProvider>
+  </>
+);
